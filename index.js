e7e73284c0cadd7129c9c767bf4cb1db6e78858f
Stella P <126360693+Stellouche@users.noreply.github.com>
2023-09-22 22:54:34 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var sidenav = document.getElementById("Sidenav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Ouvre le menu /width : 100% */
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+/* Ferme le menu / width: 0 */
+function closeNav() {
+  sidenav.classList.remove("active");
+}
+
