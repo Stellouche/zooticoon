ada29612c4783fca7d50236e1745fc0f31cb27f3
Stellouche <Stellouche@users.noreply.github.com>
2023-10-01 13:08:32 +0200

    spe

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var sidenav = document.getElementById("Sidenav");
 var openBtn = document.getElementById("openBtn");
 var closeBtn = document.getElementById("closeBtn");
+var titre1 = document.getElementById("titre1");
+var titre1t = document.getElementById("titre1t");
+var titre1w = document.getElementById("titre1w");
 
 
 openBtn.onclick = openNav;
@@ -9,11 +12,16 @@ closeBtn.onclick = closeNav;
 /* Ouvre le menu /width : 100% */
 function openNav() {
   sidenav.classList.add("active");
-
+  titre1.classList.add("active");
+  titre1t.classList.add("active");
+  titre1w.classList.add("active");
 }
 
 /* Ferme le menu / width: 0 */
 function closeNav() {
   sidenav.classList.remove("active");
+  titre1.classList.remove("active");
+  titre1t.classList.remove("active");
+  titre1w.classList.remove("active");
 }
 
